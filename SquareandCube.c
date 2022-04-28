5779ee9a13d8aaad27b6e2a714b4b8e5fe101272
mkgirach <mkgirach@gmail.com>
2022-04-28 19:19:05 +0500

    Update SquareandCube.c

--- SquareandCube.c
+++ SquareandCube.c
@@ -6,8 +6,9 @@ int main(){
 	printf("Enter the Number: ");
 	scanf("%d",&num1);
 	
-	
+	// PRINTING SQUARE
 	printf("\n\n Square: %d",(num1*num1));
+	//PRINTING CUBE
 	printf("\n\nCube: %d",(num1*num1*num1));
 	
 }
