f1ee6c5b217948f03a97a212872d5240a9ca93cd
mkgirach <mkgirach@gmail.com>
2022-04-28 19:46:52 +0500

    Update Even_and_Odd.c

--- Even_and_Odd.c
+++ Even_and_Odd.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main(){
-	
+	// declear Integer
 	int num;
 	printf("enter the number: ");
 	scanf("%d",&num);
-	
+	// number %d 2 if reminder is 0 its means Even Number otherwise off number
 	if(num%2==0){
+		// printing Even
 		printf("Even");
 	}
 	else {
+		// printing Odd
 		printf("Odd");
 	}
 }
