94d64afb93e1dd7ec3c2409b408d84818f63bec9
mkgirach <mkgirach@gmail.com>
2022-04-28 19:17:15 +0500

    This programming finding Square and Cube (in C Programming)

---
+++ SquareandCube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num1;
+	printf("Enter the Number: ");
+	scanf("%d",&num1);
+	
+	
+	printf("\n\n Square: %d",(num1*num1));
+	printf("\n\nCube: %d",(num1*num1*num1));
+	
+}
